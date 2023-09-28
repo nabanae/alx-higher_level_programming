fdaee7b1debba4504550ba5d0e11368673f4626d
nabanae <alhanlala@gmail.com>
2023-09-28 06:15:26 -0700

    done

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# take in a URL, send a request to that URL, display the size of the body of the response
-curl -sI GET "$1" | grep -i "Content-Length" | cut -d " " -f2
+# Bash script that takes in a URL, sends a request to that URL and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | awk '{print $2}'
