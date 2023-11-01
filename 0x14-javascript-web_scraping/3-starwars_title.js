12b4bb17558ce09123c5cf6a11d09af75e49ae5d
nabanae <alhanlala@gmail.com>
2023-11-01 07:30:03 -0700

    correction

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,11 +1,16 @@
 #!/usr/bin/node
 
-const myArray = process.argv.slice(2);
 const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.hbtn.io/api/films/';
 
-if (Number.isInteger(parseInt(myArray[0]))) {
-  request('http://swapi.co/api/films/' + myArray[0], function (error, response, body) {
-    if (error) console.error('error:', error); // Print the error if one happened
-    console.log(JSON.parse(body).title);
-  });
-}
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
+  }
+});
